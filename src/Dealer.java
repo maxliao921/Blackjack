25423aef3063c87633318831af6343585b1190dd
Max Liao <liaomax000@isd284.com>
2018-05-16 12:45:06 -0500

    Update Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,95 +1,91 @@
 package Blackjack;
 import java.util.ArrayList;
 import java.util.Arrays;
-/*
- * Creates a dealer that the user plays against.
- */
+
+// Creates the dealer
 class Dealer {
-ArrayList<Card> hand;//represents the dealer's hand
-private int handvalue=0;//value of the dealer's hand (starts at 0)
-private Card[] aHand;//used to convert the dealer's hand to an array
-private int AceCounter;//counts the aces in the dealer's hand
-Dealer(Deck deck)
-{
-    hand = new ArrayList<>();
-    aHand = new Card[]{};
-    int AceCounter=0;
-    for(int i=0; i<2; i++)
+    // Variables
+    ArrayList<Card> hand; // The dealer's hand
+    private int handvalue = 0; // Value of dealer's hand
+    private Card[] aHand; // Convert the dealer's hand to an array
+    private int AceCounter; // Counts aces in dealer's hand
+
+    Dealer(Deck deck)
+    {
+        hand = new ArrayList<>();
+        aHand = new Card[]{};
+        int AceCounter = 0;
+        for (int i = 0; i < 2; i++)
+        {
+            hand.add(deck.drawCard());
+        }
+        aHand = hand.toArray(aHand);
+        for(int i = 0; i < aHand.length; i++)
+        {
+            handvalue += aHand[i].getValue();
+            if(aHand[i].getValue() == 11)
+            {
+                AceCounter++;
+            }
+            while(AceCounter > 0 && handvalue > 21)
+            {
+                handvalue -= 10;
+                AceCounter--;
+            }
+        }
+    }
+
+    // Prints out dealer's first card
+    public void showFirstCard()
+    {
+        Card[] firstCard = new Card[]{};
+        firstCard = hand.toArray(firstCard);
+        System.out.println("["+firstCard[0]+"]");
+    }
+
+    // Gives dealer another card, updates value
+    public void Hit(Deck deck)
     {
         hand.add(deck.drawCard());
-    }
-    aHand = hand.toArray(aHand);
-    for(int i=0; i<aHand.length; i++)
-    {
-        handvalue += aHand[i].getValue();
-        if(aHand[i].getValue()==11)
+        aHand = hand.toArray(aHand);
+        handvalue = 0;
+        for(int i = 0; i < aHand.length; i++)
         {
-            AceCounter++;
-        }
-        while(AceCounter>0 && handvalue>21)
-        {
-            handvalue-=10;
-            AceCounter--;
-        }
-    }
-}
-/*
- * Prints the dealer's first card (the card face up at the beginning of a blackjack game).
- */
-public void showFirstCard()
-{
-    Card[] firstCard = new Card[]{};
-    firstCard = hand.toArray(firstCard);
-    System.out.println("["+firstCard[0]+"]");
-}
-/*
- * Gives the dealer another card and updates the value of his hand. Takes into account the value of aces.
- */
-public void Hit(Deck deck)
-{
-    hand.add(deck.drawCard());
-    aHand = hand.toArray(aHand);
-    handvalue = 0;
-    for(int i=0; i<aHand.length; i++)
-    {
-        handvalue += aHand[i].getValue();
-        if(aHand[i].getValue()==11)
-        {
-            AceCounter++;
-        }
-        while(AceCounter>0 && handvalue>21)
-        {
-            handvalue-=10;
-            AceCounter--;
+            handvalue += aHand[i].getValue();
+            if(aHand[i].getValue() == 11)
+            {
+                AceCounter++;
+            }
+            while(AceCounter > 0 && handvalue > 21)
+            {
+                handvalue -= 10;
+                AceCounter--;
+            }
         }
     }
-}
-/*
- * Determines if the dealer wants to hit according to classic Blackjack rules.
- */
-public boolean wantsToHit()
-{
-    if(handvalue<17)
+
+    // Determines whether dealer wants to hit or not
+    public boolean wantsToHit()
     {
-        return true;
+        if(handvalue < 17)
+        {
+            return true;
+        }
+        return false;
     }
-    return false;
-}
-/*
- * Returns true if the dealer has blackjack.
- */
-public boolean hasBlackJack()
-{
-    if(hand.size()==2 && handvalue==21)
+
+    // Message when dealer has 21
+    public boolean hasBlackJack()
     {
-        System.out.println("The dealer has blackjack!");
-        return true;
+        if(hand.size() == 2 && handvalue == 21)
+        {
+            System.out.println("The dealer has blackjack!");
+            return true;
+        }
+        return false;
     }
-    return false;
-}
-/*
- * Prints the dealer's hand.
- */
+
+    // Print dealer's hand
 public void showHand()
 {
     System.out.println(hand);
