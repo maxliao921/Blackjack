923f775d0197a1f69709aa1f6a5666d53a794fb2
Max Liao <liaomax000@isd284.com>
2018-05-16 13:45:08 -0500

    Update BlackjackMain.java

--- src/BlackjackMain.java
+++ src/BlackjackMain.java
@@ -1,157 +1,174 @@
 package Blackjack;
 import java.util.*;
+
 public class BlackjackMain {
-private static int cash;//cash the user bets with
-private static int bet;//how much the user wants to bet
-private static int AceCounter;//how many aces are in the user's hand
-private static ArrayList<Card> hand;//represents the user's hand
-private static int handvalue;//the value of the user's hand
-private static String name;//name of the user
-public static void main(String[] args){
-    System.out.println("Hi! What is your name?");
-    Scanner scan = new Scanner(System.in);
-    name = scan.nextLine();
-    System.out.println("Hello, "+name+", lets play some BlackJack!");
-    System.out.println("How much cash do you want to start with?");
-    Scanner money = new Scanner(System.in);
-    cash = money.nextInt();
-    System.out.println("You start with cash: "+cash);
-    while(cash>0){
-        Deck deck = new Deck();//initialize deck, dealer, hands, and set the bet.
-        deck.shuffle();
-        AceCounter=0;
-        Dealer dealer = new Dealer(deck);
-        List<Card> hand = new ArrayList<>();
-        hand.add(deck.drawCard());
-        hand.add(deck.drawCard());
-        System.out.println("How much would you like to bet?");
-        bet=Bet(cash);
-        System.out.println("Cash:"+(cash-bet));
-        System.out.println("Money on the table:"+bet);
-        System.out.println("Here is your hand: ");
-        System.out.println(hand);
-        int handvalue = calcHandValue(hand);
-        System.out.println("The dealer is showing: ");
-        dealer.showFirstCard();
-        if(hasBlackJack(handvalue) && dealer.hasBlackJack())//check if both the user and dealer have blackjack.
+    private static int cash; // Cash in user's hand
+    private static int bet; // How much the user is betting
+    private static int AceCounter; // How many aces the user has
+    private static ArrayList<Card> hand; // The user's hand
+    private static int handvalue; // Value of the user's hand
+    private static String name; // The username
+
+    public static void main(String[] args) {
+        System.out.println("Hi! What is your name?");
+        Scanner scan = new Scanner(System.in);
+        name = scan.nextLine();
+        System.out.println("Hello, " + name + ", lets play some BlackJack!");
+        System.out.println("How much cash do you want to start with?");
+        Scanner money = new Scanner(System.in);
+        cash = money.nextInt();
+        System.out.println("You start with cash: " + cash);
+        while(cash > 0)
         {
-            Push();
-        }
-        else if(hasBlackJack(handvalue))//check if the user has blackjack.
-        {
-            System.out.println("You have BlackJack!");
-            System.out.println("You win 2x your money back!");
-            cash=cash+bet;
-            Win();
-        }
-        else if(dealer.hasBlackJack())//check if the dealer has blackjack.
-        {
-            System.out.println("Here is the dealer's hand:");
-            dealer.showHand();
-            Lose();
-        }
-        else
-        {
-            if(2*bet<cash)//check if the user can double down.
+            // Initialize variables, set bet
+            Deck deck = new Deck();
+            deck.shuffle();
+            AceCounter=0;
+            Dealer dealer = new Dealer(deck);
+            List<Card> hand = new ArrayList<>();
+            hand.add(deck.drawCard());
+            hand.add(deck.drawCard());
+            System.out.println("How much would you like to bet?");
+            bet = Bet(cash);
+            System.out.println("Cash:" + (cash-bet));
+            System.out.println("Money on the table:" + bet);
+            System.out.println("Here is your hand: ");
+            System.out.println(hand);
+            int handvalue = calcHandValue(hand);
+            System.out.println("The dealer is showing: ");
+            dealer.showFirstCard();
+            // Check the user and dealer for blackjack
+            if(hasBlackJack(handvalue) && dealer.hasBlackJack())
             {
-                System.out.println("Would you like to double down?");//allows the user to double down.
-                Scanner doubledown = new Scanner(System.in);
-                String doubled = doubledown.nextLine();
-                while(!isyesorno(doubled))
-                {
-                    System.out.println("Please enter yes or no.");
-                    doubled = doubledown.nextLine();
-                }
-                if(doubled.equals("yes"))
-                {
-                    System.out.println("You have opted to double down!");
-                    bet=2*bet;
-                    System.out.println("Cash:"+(cash-bet));
-                    System.out.println("Money on the table:"+bet);
-                }
+                Push();
             }
-            System.out.println("Would you like to hit or stand?");//ask if the user will hit or stand
-            Scanner hitorstand = new Scanner(System.in);
-            String hitter = hitorstand.nextLine();
-            while(!isHitorStand(hitter))
+            // Check user
+            else if(hasBlackJack(handvalue))
             {
-                System.out.println("Please enter 'hit' or 'stand'.");
-                hitter = hitorstand.nextLine();
+                System.out.println("You have BlackJack!");
+                System.out.println("You win 2x your money back!");
+                cash = cash + bet;
+                Win();
             }
-            while(hitter.equals("hit"))//hits the user as many times as he or she pleases.
+            // Check dealer
+            else if(dealer.hasBlackJack())
             {
-                Hit(deck, hand);
-                System.out.println("Your hand is now:");
-                System.out.println(hand);
-                handvalue = calcHandValue(hand);
-                if(checkBust(handvalue))//checks if the user busted
-                {
-                    Lose();
-                    break;
-                }
-                if(handvalue<=21 && hand.size()==5)//checks for a five card trick.
-                {
-                    fivecardtrick();
-                    break;
-                }
-                System.out.println("Would you like to hit or stand?");
-                hitter = hitorstand.nextLine();
-            }
-            if(hitter.equals("stand"))//lets the user stand.
-            {
-                int dealerhand = dealer.takeTurn(deck);//takes the turn for the dealer.
-                System.out.println("");
                 System.out.println("Here is the dealer's hand:");
                 dealer.showHand();
-                if(dealerhand>21)//if the dealer busted, user wins.
+                Lose();
+            }
+            else
+            {
+                // Ask user about doble down
+                if(2*bet<cash)
                 {
-                    Win();
-                }
-                else
-                {
-                    int you = 21-handvalue;//check who is closer to 21 and determine winner
-                    int deal = 21-dealerhand;
-                    if(you==deal)
+                    System.out.println("Would you like to double down?");
+                    Scanner doubledown = new Scanner(System.in);
+                    String doubled = doubledown.nextLine();
+                    while(!isyesorno(doubled))
                     {
-                        Push();
+                        System.out.println("Please enter yes or no.");
+                        doubled = doubledown.nextLine();
                     }
-                    if(you<deal)
+                    if(doubled.equals("yes"))
+                    {
+                        System.out.println("You have opted to double down!");
+                        bet = 2*bet;
+                        System.out.println("Cash:" + (cash-bet));
+                        System.out.println("Money on the table:" + bet);
+                    }
+                }
+                // Ask user to hit or stand
+                System.out.println("Would you like to hit or stand?");
+                Scanner hitorstand = new Scanner(System.in);
+                String hitter = hitorstand.nextLine();
+                while(!isHitorStand(hitter))
+                {
+                    System.out.println("Please enter 'hit' or 'stand'.");
+                    hitter = hitorstand.nextLine();
+                }
+                // Allows user to keep hitting
+                while(hitter.equals("hit"))
+                {
+                    Hit(deck, hand);
+                    System.out.println("Your hand is now:");
+                    System.out.println(hand);
+                    handvalue = calcHandValue(hand);
+                    // Checks if user is busted
+                    if(checkBust(handvalue))
+                    {
+                        Lose();
+                        break;
+                    }
+                    // Checks for five card trick
+                    if(handvalue<=21 && hand.size()==5)
+                    {
+                        fivecardtrick();
+                        break;
+                    }
+                    System.out.println("Would you like to hit or stand?");
+                    hitter = hitorstand.nextLine();
+                }
+                // User stands
+                if(hitter.equals("stand"))
+                {
+                    // Dealer takes turn
+                    int dealerhand = dealer.takeTurn(deck);
+                    System.out.println("");
+                    System.out.println("Here is the dealer's hand:");
+                    dealer.showHand();
+                    // User wins if dealer is busted
+                    if(dealerhand > 21)
                     {
                         Win();
                     }
-                    if(deal<you)
+                    else
                     {
-                        Lose();
+                        // Check to see who wins
+                        int you = 21-handvalue;
+                        int deal = 21-dealerhand;
+                        if(you == deal)
+                        {
+                            Push();
+                        }
+                        if(you < deal)
+                        {
+                            Win();
+                        }
+                        if(deal < you)
+                        {
+                            Lose();
+                        }
                     }
                 }
             }
-        }
-    System.out.println("Would you like to play again?");//ask if the user wants to keep going
-    Scanner yesorno = new Scanner(System.in);
-    String answer = yesorno.nextLine();
-    while(!isyesorno(answer))
+            // Play again
+            System.out.println("Would you like to play again?");
+            Scanner yesorno = new Scanner(System.in);
+            String answer = yesorno.nextLine();
+            while(!isyesorno(answer))
+                {
+                    System.out.println("Please answer yes or no.");
+                    answer = yesorno.nextLine();
+                }
+            if(answer.equals("no"))
             {
-                System.out.println("Please answer yes or no.");
-                answer = yesorno.nextLine();
+                break;
             }
-    if(answer.equals("no"))
-    {
-        break;
+        }
+        // Shows winnings if user stops playing
+        System.out.println("Your cash is: " + cash);
+        if(cash == 0)
+        {
+            System.out.println("You ran out of cash!");
+        }
+        else
+        {
+            System.out.println("Enjoy your winnings, " + name + "!");
+        }
     }
-}
-    System.out.println("Your cash is: "+cash);//if user doesn't want to play or runs out of cash, either congratulates them on their winnings or lets them know
-    if(cash==0)
-    {
-        System.out.println("You ran out of cash!");
-    }
-    else
-    {
-        System.out.println("Enjoy your winnings, "+name+"!");
-    }
-}
-/*
- * Checks if the user has blackjack.
- */
+
+    // Check user for blackjack
 public static boolean hasBlackJack(int handValue)
 {
     if(handValue==21)
