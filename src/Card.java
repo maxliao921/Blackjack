22fe130b1064539903372bf12ca0bb169f5c36b8
Max Liao <liaomax000@isd284.com>
2018-05-16 12:34:39 -0500

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,69 +1,62 @@
 package Blackjack;
+import java.util.*;
 
+// Creates playing card
 class Card {
-/*
- * Creates a playing card.
- */
-private int rank;//represents the rank of a card
-private int suit;//represents the suit of a card
-private int value;//represents the value of a card
-private static String[] ranks = {"Joker","Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
-private static String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
-/*
- * Created with an integer that represents a spot in the String array ranks and the String array suits. This represents
- * the rank and suit of an individual card.
- */
-Card(int suit, int values)
-{
-    this.rank=values;
-    this.suit=suit;
-}
-/*
- * Returns the string version of a card.
- */
-public String toString()
-{
-    return ranks[rank]+" of "+suits[suit];
-}
-/*
- * Returns the rank of a card.
- */
-public int getRank()
-{
-    return rank;
-}
-/*
- * Returns the suit of a card.
- */
-public int getSuit()
-{
-    return suit;
-}
-/*
- * Returns the value of a card. If a jack, queen, or king the value is ten. Aces are 11 for now.
- */
-public int getValue()
-{
-    if(rank>10)
+    // Variables
+    private int rank;//represents the rank of a card
+    private int suit;//represents the suit of a card
+    private int value;//represents the value of a card
+    private static String[] ranks = {"Joker","Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
+    private static String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
+
+    // Constructor
+    Card(int suit, int values)
     {
-        value=10;
+        this.rank = values;
+        this.suit = suit;
     }
-    else if(rank==1)
+
+    // Returns card as string
+    public String toString()
     {
-        value=11;
+        return ranks[rank]+" of "+suits[suit];
     }
-    else
+    
+    // Returns card rank
+    public int getRank()
     {
-        value=rank;
+        return rank;
     }
-    return value;
-}
-/*
- * Sets the value of a card.
- */
-public void setValue(int set)
-{
-    value = set;
-}
+    
+    // Returns card suit
+    public int getSuit()
+    {
+        return suit;
+    }
+
+    // Returns value of card, 11 for jack, queen, king, or ace
+    public int getValue()
+    {
+        if(rank>10)
+        {
+            value=10;
+        }
+        else if(rank==1)
+        {
+            value=11;
+        }
+        else
+        {
+            value=rank;
+        }
+        return value;
+    }
+
+    // Sets card value
+    public void setValue(int set)
+    {
+        value = set;
+    }
 
 }
